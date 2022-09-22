c31112052a64f05ffb6fcc74fad9a9270b862dcc
hengxin(Hengfeng Wei) <hfwei@nju.edu.cn>
2022-09-22 20:50:48 +0800

    0-intro: README

---
+++ 2022-CPL/hello.c
@@ -0,0 +1,18 @@
+//
+// Created by hfwei on 2022/9/21.
+//
+
+/**
+ * Created by hfwei on 2022/9/21.
+ */
+
+#include <stdio.h>
+
+/**
+ * main function
+ */
+int main() {
+  printf("Hello World\n");
+
+  return 0;
+}
